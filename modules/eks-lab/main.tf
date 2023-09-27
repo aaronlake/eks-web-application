547ed49badeea1399c1a340f09d334219a2adc15
Aaron Lake <aaron.lake@gmail.com>
2023-09-27 10:47:56 -0400

    chore: cleanup

--- modules/eks-lab/main.tf
+++ modules/eks-lab/main.tf
@@ -21,6 +21,18 @@
 * 9. And more!
 *
 */
+
+locals {
+  env_map = {
+    "Development" = "dev"
+    "Production"  = "prod"
+    "Demo"        = "demo"
+  }
+
+  env  = local.env_map[var.common_tags["Environment"]]
+  name = lower(replace(var.common_tags["Application"], " ", ""))
+}
+
 terraform {
   required_version = ">=1.5.7"
 
